2942f4bd5501783c7d732eb139c06e24e1623a3e
sonal <sonalpriya655@gmail.com>
2021-01-11 16:27:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  encapsulation:ViewEncapsulation.None,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'spreadsheet';
+
+  closeResult: string;
+
+  constructor(private modalService: NgbModal){}
+
+  openXl(content){
+    this.modalService.open(content,{size:'xl'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import {SpreadsheetAllModule} from '@syncfusion/ej2-angular-spreadsheet';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    SpreadsheetAllModule
   ],
   providers: [],
   bootstrap: [AppComponent]
